45dfb6b49804baf3c2cfde62717be86af4c8bd7a
pranto mollick <prantocse01@gmail.com>
2022-06-19 02:16:55 +0600

    boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,17 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const environment = require('./config/environment');
 
+const templateFiles = fs
+  .readFileSync(environment.paths.source)
+  .filter((file) =>
+    [".html", ".ejs"].includes(path.extname(file).toLowerCase())
+  )
+  .map((filename) => ({
+    input: filename,
+    output: filename.replace(/\.ejs$/, ".html"),
+  }));
+
+
 
 module.exports = {
     entry: {
@@ -17,5 +28,45 @@ module.exports = {
     output: {
         filename: 'js/[name].js',
         path: environment.paths.output,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.(s|sc|c)ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }, 
+            {
+                test: /\.(png|gif|jpe?g|svg)$/i,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: environment.limits.images,
+                    },
+                }, 
+                generator: {
+                    filename: 'images/design/[name].[hash:6][ext]',
+                },
+
+            }, 
+
+            {
+                test: /\.(eot|ttf|woff|woff2)$/,
+                type: 'asset', 
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: environment.limits.fonts
+                    }
+                },
+                generator: {
+                    filename: 'fonts/[name].[hash:6][ext]',
+                }
+            }
+        ]
     }
 };
